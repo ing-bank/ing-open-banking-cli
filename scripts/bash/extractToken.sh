24c92adb60caab6d5fa8f316d4920f597de815f8
Alexandru Ionut Balan <alexandru.ionut.balan97@gmail.com>
2024-10-30 16:38:41 +0200

    fix: extractToken now uses jq to extract the exact field

--- scripts/bash/extractToken.sh
+++ scripts/bash/extractToken.sh
@@ -2,10 +2,6 @@
 
 read -r INPUT_JSON
 
-IFS="," read -r -a JSON_PARTS <<<"$INPUT_JSON"
-IFS=":" read -r -a ACCESS_TOKEN_LINE <<<"${JSON_PARTS[0]}"
-unset IFS
+temp=$(echo "$INPUT_JSON" | jq -r '.access_token')
 
-temp="${ACCESS_TOKEN_LINE[1]%\"}"
-temp="${temp#\"}"
 echo "$temp"
